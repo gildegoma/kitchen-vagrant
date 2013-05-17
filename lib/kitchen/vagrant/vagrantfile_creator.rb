737627fda13f8ebb8d96f55666d0a4d833f78341
Gilles Cornu <git@gilles.cornu.name>
2013-05-17 11:29:10 +0200

    Copy-Paste 'cherry-pick' PR23 from manul
    see https://github.com/opscode/kitchen-vagrant/pull/23

--- lib/kitchen/vagrant/vagrantfile_creator.rb
+++ lib/kitchen/vagrant/vagrantfile_creator.rb
@@ -52,6 +52,7 @@ module Kitchen
         arr << %{  c.vm.box_url = "#{config[:box_url]}"} if config[:box_url]
         arr << %{  c.vm.hostname = "#{instance.name}.vagrantup.com"}
         arr << %{  c.ssh.username = "#{config[:username]}"} if config[:username]
+        arr << %{  c.ssh.private_key_path = "#{config[:ssh_key]}"} if config[:ssh_key]
       end
 
       def network_block(arr)
